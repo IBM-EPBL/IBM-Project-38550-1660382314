b392bedbb2eb7ef6137293fec1e2d7683f014bd4
sasikaladevi123 <117064008+sasikaladevi123@users.noreply.github.com>
2022-11-25 19:38:54 -0800

    Add files via upload

---
+++ Project Development Phase/sprint 1/sprint 3/static/application features/bottomtabnavbar.js
@@ -0,0 +1,13 @@
+function tabUser(){
+    // alert("tab user dharun")
+    // alert("hello ")
+}
+const myFunc = () => {
+    const tabfooter = document.getElementById("tabfooter");
+    const input = document.getElementById("searchinput");
+    input.focus();
+    tabfooter.style.display ='none'
+    input.select()
+ tabfooter.style.display ='block'
+
+ }
